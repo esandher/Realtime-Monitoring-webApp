e250b7ed482c455afa5d3bf51d9f0cac329355b8
esandher <e.sandovalh@uniandes.edu.co>
2023-09-12 21:43:23 -0500

    Update urls.py

--- realtimeMonitoring/realtimeGraph/urls.py
+++ realtimeMonitoring/realtimeGraph/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("historical/data", download_csv_data, name="historical-data"),
+    path("getdata/", getJSONdata.as_view(), name="getdata"),
 ]
